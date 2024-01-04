3b5d2a12e037602286e9f82bbd1251e568403874
Adrian Savov <skandalno@icloud.com>
2024-01-04 11:13:09 +0200

    feat: Add MyWork component
    and its style and assets

---
+++ src/Components/MyWorkComponent/MyWork.jsx
@@ -0,0 +1,77 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import "../../PageTransition.css";
+import "./MyWork.css";
+
+function MyWork() {
+  return (
+    <div className="page">
+      <div className="card-container">
+        <Card>
+          <Card.Img variant="top" src="logo.jpg"/>
+          <Card.Body>
+            <Card.Title>A-Mobile</Card.Title>
+            <Card.Text className="card-text-contain">
+              A-Mobile is a web application to manage an online smart devices
+              store.
+            </Card.Text>
+            <div className="card-btn">
+              <Button
+                variant="primary"
+                as={Link}
+                to="https://github.com/AdrianSavov/A-Mobile/tree/main/app"
+              >
+                GitHub Link
+              </Button>
+              <Button
+                variant="primary"
+                as={Link}
+                to="https://project-defence.web.app/"
+              >
+                Project Link
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="404.png" />
+          <Card.Body>
+            <Card.Title>Coming Soon...</Card.Title>
+            <Card.Text className="card-text-contain">As long as there is more projects, you'll see them listed here.</Card.Text>
+            <div className="card-btn">
+              <Button variant="primary">GitHub Link</Button>
+              <Button variant="primary">Project Link</Button>
+            </div>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="404.png" />
+          <Card.Body>
+            <Card.Title>Coming Soon...</Card.Title>
+            <Card.Text className="card-text-contain">
+            As long as there is more projects, you'll see them listed here.
+            </Card.Text>
+            <div className="card-btn">
+              <Button
+                variant="primary"
+                as={Link}
+                to="https://github.com/AdrianSavov/A-Mobile/tree/main/app"
+              >
+                GitHub Link
+              </Button>
+              <Button
+                variant="primary"
+                as={Link}
+                to="https://project-defence.web.app/"
+              >
+                Project Link
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
+}
+export default MyWork;
--- src/RoutesConfig/RoutesConfig.jsx
+++ src/RoutesConfig/RoutesConfig.jsx
@@ -1,15 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../Components/Home/Home';
 import MySkills from '../Components/MySkillsComponent/MySkills';
-
+import MyWork from '../Components/MyWorkComponent/MyWork';
 
 const RoutesConfig = () => {
-
     return (
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='my-skills' element={<MySkills />} />
-
+            <Route path='my-work' element={<MyWork />} />
         </Routes>
     );
 };
