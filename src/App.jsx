47978f66f3696b9a3c76fde7fcb74454fb31c7eb
Adrian Savov <skandalno@icloud.com>
2024-01-04 11:56:26 +0200

    style (app): background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import NavbarComponent from './Components/Navbar/NavbarComponent'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Components/Footer/Footer';
 import RoutesConfig from './RoutesConfig/RoutesConfig';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-
+  const backgroundImage = {
+    backgroundImage: 'url("background.jpg")'
+  }
   return (
-    <div className='main'>
+    <div className='main' style={backgroundImage}>
       <NavbarComponent />
       <RoutesConfig />
       <Footer />
